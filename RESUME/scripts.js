277d5b389ee1fce5777ac0c09dce6a1b1d403341
ganiaschristine01 <ganiaschristine@gmail.com>
2025-01-23 10:38:34 +0800

    Delete RESUME directory

--- RESUME/scripts.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById("colorChangeButton");
-button.addEventListener("click", changeColor);
-
-function changeColor() {
-    const resume = document.querySelector(".resume");
-    const color = "#ca699e"
-    resume.style.backgroundColor = color;
-}
